1708df292f32af1aa9549e76641fbee33d0a49cc
JulianCastill0 <juli.castillo909@gmail.com>
2023-09-12 01:00:03 -0300

    Logica andando perfecta, falta diseno

--- main.js
+++ main.js
@@ -1,41 +1,11 @@
-const url = "https://crudcrud.com/api/adc54d7744e946cd8ffc1851accabb6d/prueba000";
-
-
 document.addEventListener("DOMContentLoaded", () => {
+    const url = "https://crudcrud.com/api/adc54d7744e946cd8ffc1851accabb6d/prueba000";
+    const enviar = document.getElementById("enviar");
+    const borraTodo = document.getElementById("borraTodo");
+    const mostrarTodo = document.getElementById("mostrarTodo");
     let tabla = document.getElementById("cuerpoTabla");
-    const borraTodo = document.getElementById("borraTodo")
 
-    borraTodo.addEventListener("click", () => {
-        fetch(url)
-        .then(response=>response.json())
-        .then (datos => {
-            for (let i = 0; i < datos.length; i++){;
-                fetch(url + '/' + datos[i]._id, {headers: {"Content-Type": "application/json; charset=utf-8"}, method:'delete'})
-            };
-            tabla.innerHTML = "";
-        })
-    })
-
-    function agregarALista(){
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            let element = data[data.length - 1];
-            let tr = document.createElement("tr");
-            tr.innerHTML += '<td>' + element.nombre + '</td><td>' + element.apellido + '</td><td>' + element.grupo + '</td><td>' + element.sala + '</td>';
-            let button = document.createElement("button");
-            button.innerHTML = "borrar";
-            button.addEventListener("click", () => {
-                fetch(url + "/" + element._id, {method: 'DELETE'})
-                tr.remove();
-            });
-            tr.appendChild(button);
-            tabla.appendChild(tr);
-
-        })
-    };
-
-    document.getElementById("enviar").addEventListener("click", () => {
+    enviar.addEventListener("click", () => {
         let persona = {
             nombre: document.getElementById("nombre").value,
             apellido: document.getElementById("apellido").value, 
@@ -53,8 +23,61 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(response => response.json())
         .then(data => {
             Array.from(document.getElementsByClassName("campos")).forEach(element => {element.value = "";});
+            agregarALista();
         })
-
-        agregarALista();
     });
+
+    borraTodo.addEventListener("click", () => {
+        fetch(url)
+        .then(response => response.json())
+        .then (data => {
+            data.forEach(element => {
+                fetch(url + '/' + element._id, {method:'DELETE'})
+            });
+            tabla.innerHTML = "";
+        })
+    });
+
+    mostrarTodo.addEventListener("click", () => {
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            tabla.innerHTML = "";
+            data.forEach(element => {   
+                let tr = document.createElement("tr");
+                let boton = document.createElement("button");
+
+                tr.innerHTML = `<td>${element.nombre}</td><td>${element.apellido}</td><td>${element.grupo}</td><td>${element.sala}</td>`;
+                boton.innerHTML = "🗑️";
+                boton.addEventListener("click", () => {
+                    fetch(url + "/" + element._id, {method: 'DELETE'})
+                    tr.remove();
+                })
+
+                tr.appendChild(boton);
+                tabla.appendChild(tr);
+            });
+        })
+    });
+
+    function agregarALista(){
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            let element = data[data.length - 1];
+            let tr = document.createElement("tr");
+            let button = document.createElement("button");
+
+            tr.innerHTML = `<td>${element.nombre}</td><td>${element.apellido}</td><td>${element.grupo}</td><td>${element.sala}</td>`;
+            button.innerHTML = "🗑️";
+            button.addEventListener("click", () => {
+                fetch(url + "/" + element._id, {method: 'DELETE'})
+                tr.remove();
+            });
+
+            tr.appendChild(button);
+            tabla.appendChild(tr);
+
+        })
+    };
 });
