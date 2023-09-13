14fba41378028ccea2ace7f5bab26b5ed9c26fbe
beluarias <belenariasfor@gmail.com>
2023-09-13 00:58:59 -0300

    basura de cruz a img

--- main.js
+++ main.js
@@ -6,17 +6,17 @@ document.addEventListener("DOMContentLoaded", () => {
     let tabla = document.getElementById("cuerpoTabla");
 
     enviar.addEventListener("click", () => {
+        let options = {
+            headers: {"Content-Type": "application/json; charset=utf-8"}, 
+            method: "POST", 
+            body: JSON.stringify(persona)
+        };
+        
         let persona = {
             nombre: document.getElementById("nombre").value,
             apellido: document.getElementById("apellido").value, 
             grupo: document.getElementById("grupo").value, 
             sala: document.getElementById("sala").value
-        };
-
-        let options = {
-            headers: {"Content-Type": "application/json; charset=utf-8"}, 
-            method: "POST", 
-            body: JSON.stringify(persona)
         };
 
         fetch(url, options)
@@ -26,38 +26,6 @@ document.addEventListener("DOMContentLoaded", () => {
             agregarALista();
         })
     });
-
-    // PARA PONERLE ESTILO A LA ALERTA DE CONFIRMACION
-    const customConfirm = document.getElementById("custom-confirm");
-    const confirmYes = document.getElementById("confirm-yes");
-    const confirmNo = document.getElementById("confirm-no");
-    
-    borraTodo.addEventListener("click", () => {
-        // Mostrar la ventana de confirmación
-        customConfirm.style.display = "block";
-    });
-    
-    confirmYes.addEventListener("click", () => {
-        // Ejecutar la eliminación si se confirma
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(element => {
-                fetch(url + '/' + element._id, {method:'DELETE'});
-            });
-            tabla.innerHTML = "";
-        });
-    
-        // Ocultar la ventana de confirmación después de la eliminación
-        customConfirm.style.display = "none";
-    });
-    
-    confirmNo.addEventListener("click", () => {
-        // Ocultar la ventana de confirmación si se cancela
-        customConfirm.style.display = "none";
-    });
-    //  TERMINA EL PONERLE ESTILO A LA ALERTA DE CONFIRMACION
-    
     
 
     mostrarTodo.addEventListener("click", () => {
@@ -68,13 +36,16 @@ document.addEventListener("DOMContentLoaded", () => {
             data.forEach(element => {   
                 let tr = document.createElement("tr");
                 let boton = document.createElement("button");
+                let img = document.createElement("img");
 
                 tr.innerHTML = `<td>${element.nombre}</td><td>${element.apellido}</td><td>${element.grupo}</td><td>${element.sala}</td>`;
-                boton.innerHTML = "✕";
-                boton.addEventListener("click", () => {
+                
+                img.src = "basura.png";
+                img.addEventListener("click", () => {
                     borrarElemento(element._id, tr);
-                })
+                });
 
+                boton.appendChild(img); // Agregar la imagen al botón
                 tr.appendChild(boton);
                 tabla.appendChild(tr);
             });
@@ -88,13 +59,16 @@ document.addEventListener("DOMContentLoaded", () => {
             let element = data[data.length - 1];
             let tr = document.createElement("tr");
             let button = document.createElement("button");
+            let img = document.createElement("img");
 
             tr.innerHTML = `<td>${element.nombre}</td><td>${element.apellido}</td><td>${element.grupo}</td><td>${element.sala}</td>`;
-            button.innerHTML = "✕";
-            button.addEventListener("click", () => {
+            
+            img.src = "basura.png";
+            img.addEventListener("click", () => {
                 borrarElemento(element._id, tr);
             });
 
+            button.appendChild(img); // Agregar la imagen al botón
             tr.appendChild(button);
             tabla.appendChild(tr);
 
@@ -105,5 +79,38 @@ document.addEventListener("DOMContentLoaded", () => {
         fetch(url + "/" + id, {method: 'DELETE'});
         tr.remove();
     };
+
+        // PARA PONERLE ESTILO A LA ALERTA DE CONFIRMACION
+        const customConfirm = document.getElementById("custom-confirm");
+        const confirmYes = document.getElementById("confirm-yes");
+        const confirmNo = document.getElementById("confirm-no");
+        
+        borraTodo.addEventListener("click", () => {
+            // Mostrar la ventana de confirmación
+            customConfirm.style.display = "block";
+        });
+        
+        confirmYes.addEventListener("click", () => {
+            // Ejecutar la eliminación si se confirma
+            fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(element => {
+                    fetch(url + '/' + element._id, {method:'DELETE'});
+                });
+                tabla.innerHTML = "";
+            });
+        
+            // Ocultar la ventana de confirmación después de la eliminación
+            customConfirm.style.display = "none";
+        });
+        
+        confirmNo.addEventListener("click", () => {
+            // Ocultar la ventana de confirmación si se cancela
+            customConfirm.style.display = "none";
+        });
+        //  TERMINA LA ALERTA DE CONFIRMACION
+        
+
 });
 
